b21045053164b726c8ad6b044dea86b6dbecf80e
YZIlcheva21 <YZIlcheva21@codingburgas.bg>
2023-03-14 21:53:36 +0200

    Add main menu beta version

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -1,20 +1,119 @@
-// 2223-9th-grade-sprint-math-games.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h.>
+
+using namespace std;
+
+// Output coordinates manipulation function
+void outputPosition(int x, int y)
+{
+	COORD position;
+	position.X = x;
+	position.Y = y;
+	if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position))
+	{
+		return;
+	}
+}
+
+// Output color manipulation function
+void color(int color)
+{
+	if (SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color))
+	{
+		return;
+	}
+}
+
+void printMenuOptions(string menuOptions[], int selectedOption)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		// Print arrow before selected option
+		if (i == selectedOption)
+		{
+			outputPosition(1, i + 1);
+			cout << "-> " << menuOptions[i];
+		}
+
+		// Print space before not selected option
+		else
+		{
+			outputPosition(1, i + 1);
+			cout << "   " << menuOptions[i];
+		}
+	}
+}
+
+void mainMenu(string menuOptions[])
+{
+	int selectedOption = 0; // By default when you open the game you are hovering the first[0] option
+	char pressedKey = ' ';
+	bool exitStatement = true;
+
+	printMenuOptions(menuOptions, selectedOption);
+
+	while (exitStatement)
+	{
+		pressedKey = _getch();
+
+		// Moving up through the menu
+		if (selectedOption != 0 && pressedKey == (char)72)
+		{
+			selectedOption--;
+		}
+
+		// Moving down through the menu
+		if (selectedOption != 2 && pressedKey == (char)80)
+		{
+			selectedOption++;
+		}
+
+		printMenuOptions(menuOptions, selectedOption);
+
+		// Choosing menu option
+		if (pressedKey == '\r')
+		{
+			switch (selectedOption)
+			{
+				// Start game
+			case 0:
+				system("CLS");
+				cout << "Start";
+				break;
+
+				// Rules
+			case 1:
+				system("CLS");
+				cout << "Rules";
+				break;
+
+				// Exit
+			case 2:
+				exitStatement = false;
+				system("CLS");
+				cout << "Press any key to exit";
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name;
+	cout << "Hello, please enter your name here: ";
+	cin >> name;
+
+	system("CLS");
+
+	string mainMenuOptions[] = { "Start", "Rules", "Exit" }; // String array with all the main menu options
+	int selectedOption = 0;
+
+	mainMenu(mainMenuOptions);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
