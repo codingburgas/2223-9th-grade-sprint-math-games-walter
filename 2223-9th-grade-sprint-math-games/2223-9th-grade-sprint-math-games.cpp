0b9db5ddf9f22b4b0494f009ae0664a9d18f6b6c
YZIlcheva21 <YZIlcheva21@codingburgas.bg>
2023-03-18 11:41:23 +0200

    Add game levels' menu

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+// Up - 72
+// Down - 80
+// Left - 75
+// Right - 77
+
+
 // Output coordinates manipulation function
 void outputPosition(int x, int y)
 {
@@ -24,6 +30,137 @@ void color(int color)
 	if (SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color))
 	{
 		return;
+	}
+}
+
+void printGameLevels(int selectedLevel)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		if (selectedLevel == i)
+		{
+			// Change selected option color
+			color(4);
+			outputPosition(2 * i, 1);
+			cout << i + 1 << " ";
+		}
+
+		else
+		{
+			color(7);
+			outputPosition(2 * i, 1);
+			cout << i + 1 << " ";
+		}
+	}
+}
+
+void firstLevel()
+{
+	// Game level
+	cout << "Bitwise AND ""&""" << endl;
+}
+
+void secondLevel()
+{
+	// Game level
+}
+
+void thirdLevel()
+{
+	// Game level
+}
+
+void fourthLevel()
+{
+	// Game level
+}
+
+void fifthLevel()
+{
+	// Game level
+}
+
+void sixthLevel()
+{
+	// Game level
+}
+
+void seventhLevel()
+{
+	// Game level
+}
+
+void eighthLevel()
+{
+	// Game level
+}
+
+void ninethLevel()
+{
+	// Game level
+}
+
+void tenthtLevel()
+{
+	// Game level
+}
+
+void game()
+{
+	// By default you are hovering the first level
+	int selectedLevel = 0;
+	char pressedKey = ' ';
+
+	printGameLevels(selectedLevel);
+
+	bool exitStatement = true;
+
+	while (exitStatement)
+	{
+		pressedKey = _getch();
+
+		// Moving up through the levels
+		if (selectedLevel != 0 && pressedKey == (char)75)
+		{
+			selectedLevel--;
+		}
+
+		// Moving down through the levels
+		if (selectedLevel != 9 && pressedKey == (char)77)
+		{
+			selectedLevel++;
+		}
+
+		printGameLevels(selectedLevel);
+
+		// Choosing level
+		if (pressedKey == '\r')
+		{
+			switch (selectedLevel)
+			{
+				// Start level
+			case 0:
+				system("CLS");
+				game();
+				break;
+
+				// Rules
+			case 1:
+				system("CLS");
+				cout << "Rules";
+				break;
+
+				// Exit
+			case 2:
+				exitStatement = false;
+				system("CLS");
+				cout << "Press any key to exit";
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 }
 
@@ -49,7 +186,8 @@ void printMenuOptions(string menuOptions[], int selectedOption)
 
 void mainMenu(string menuOptions[])
 {
-	int selectedOption = 0; // By default when you open the game you are hovering the first[0] option
+	// By default when you open the game you are hovering the first option
+	int selectedOption = 0;
 	char pressedKey = ' ';
 	bool exitStatement = true;
 
@@ -81,7 +219,7 @@ void mainMenu(string menuOptions[])
 				// Start game
 			case 0:
 				system("CLS");
-				cout << "Start";
+				game();
 				break;
 
 				// Rules
@@ -113,7 +251,6 @@ int main()
 	system("CLS");
 
 	string mainMenuOptions[] = { "Start", "Rules", "Exit" }; // String array with all the main menu options
-	int selectedOption = 0;
 
 	mainMenu(mainMenuOptions);
 }
