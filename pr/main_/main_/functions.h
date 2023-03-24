92f61562ecc22ad217428734e87b3972d19cdca7
ISDelchilov21 <ISDelchilov21@codingburgas.bg>
2023-03-24 20:29:42 +0200

    Add main the game and the main menu.

---
+++ pr/main_/main_/functions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void mainGame(bool* isClosed);
+bool IsAnyKeyPressed();
+void mainMenu(bool* game, bool* isClosed);
---
+++ pr/main_/main_/main_.cpp
@@ -0,0 +1,45 @@
+﻿#include "raylib.h"
+#include "functions.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int screenWidth = 1000;
+const int screenHeight = 500;
+
+
+
+void mainSettings()
+{
+
+}
+
+int main()
+{
+	bool isClosed = false;
+	bool game = false;
+	int temp = 0;
+
+	InitWindow(screenWidth, screenHeight, "NAME");
+		
+	SetTargetFPS(60);
+	
+
+	while (!isClosed)
+	{
+		if (game == true)
+		{
+			mainGame(&isClosed);
+		}
+		else
+		{
+			mainMenu(&game, &isClosed);
+		}
+	}
+	
+	        
+	CloseWindow();
+		
+	
+}
